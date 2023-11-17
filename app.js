9b64802b9f5b6a0d79b36cbb6c053e3a25e9d360
jjacque1 <jacksonjacque.jj@gmail.com>
2023-11-16 22:31:49 -0500

    OMDb API UI completed/ starting API

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 //API: https://www.omdbapi.com/?apikey=bef2d4c&s=fast
 
-// async function main() {
-//     const movies = await fetch("https://www.omdbapi.com/?apikey=bef2d4c&s=fast");
-//     const moviesData = await movies.json();
-//     console.log(moviesData);
-// }
+async function main() {
+    const movies = await fetch("https://www.omdbapi.com/?apikey=bef2d4c&s=fast");
+    const moviesData = await movies.json();
+    console.log(moviesData);
+}
 
-// main();
+main();
