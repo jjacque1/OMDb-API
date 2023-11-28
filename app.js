5b484ee0f576a7c37b6e1c0cefbd3526b0dc5fdb
jjacque1 <jacksonjacque.jj@gmail.com>
2023-11-28 01:06:51 -0500

    unsuccessful! start over

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 //API: https://www.omdbapi.com/?apikey=bef2d4c&s=fast
 
 
+const movieListEl = document.querySelector(".movie__list");
 async function main() {
-  const movies = await fetch("https://www.omdbapi.com/?apikey=bef2d4c&s=fast");
+  const movies = await fetch("https://www.omdbapi.com/?apikey=bef2d4c&s=more");
   const moviesData = await movies.json();
   
-  console.log(moviesData)
- 
 }
 
 main();
+
