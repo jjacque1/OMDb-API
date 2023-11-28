ec260a06fc15da51e1c5a526a9ae35d4692bff82
jjacque1 <jacksonjacque.jj@gmail.com>
2023-11-28 00:49:23 -0500

    still working

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 //API: https://www.omdbapi.com/?apikey=bef2d4c&s=fast
 
+
 async function main() {
   const movies = await fetch("https://www.omdbapi.com/?apikey=bef2d4c&s=fast");
   const moviesData = await movies.json();
+  
   console.log(moviesData)
+ 
 }
 
 main();
