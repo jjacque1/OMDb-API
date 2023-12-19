b7863f26d0689a967a5feb3e3f5923c4a62f1f0d
jjacque1 <jacksonjacque.jj@gmail.com>
2023-12-19 00:46:50 -0500

    onkeyup works successfully

--- app.js
+++ app.js
@@ -4,8 +4,6 @@
 let movieNameRef = document.getElementById("movie-name");
 let searchBtn = document.getElementById("movie__search-btn");
 let result = document.getElementById("result");
-let resultFastMovies = document.getElementById("movieFast");
-
 
 async function main() {
   let movieName = movieNameRef.value;
@@ -13,9 +11,6 @@ async function main() {
   let url = `http://www.omdbapi.com/?t=${movieName}&apikey=bef2d4c`;
   const movies = await fetch(url);
   const moviesData = await movies.json();
-
-  
-  
 
   if (moviesData.Response === "True") {
     result.innerHTML = `<div class="result_grid" id="result_grid">
@@ -47,7 +42,7 @@ async function main() {
       <p class="language"><b>Language:</b> ${moviesData.Language}</p>
       <p class="awards"><b>Award:</b> ${moviesData.Awards}</p>
       <p class="boxoffice"><b>Box Office:</b> ${moviesData.BoxOffice}</p>
-      <p class="ratings"><b>Ratings:</b> ${moviesData.Ratings[0].Value}</p>
+      
       <p class="runtime"><b>Runtime:</b> ${moviesData.Runtime}</p>
       <p class="director"><b>Director:</b> ${moviesData.Director}</p>
     </div>
@@ -62,38 +57,27 @@ async function main() {
 main();
 
 
-// async function filterMovies(event) {
-//   const moviesFilter = await fetch("");
-//   const moviesFilterData = await moviesFilter.json();
+
+//API: https://www.omdbapi.com/?apikey=bef2d4c&s=fast
+// let resultFastMovies = document.getElementById("movieFast");
+
+// async function allFastMovies() {
+//   const movieFast = await fetch("https://www.omdbapi.com/?apikey=bef2d4c&s=fast");
+//   const movieFastData = await movieFast.json();
+
+//   const movieshtml = movieFastData.Search.map(movieFast => ` <div class="movie__img--wrapper">
+//   //   <img
+//   //     class="movie__img"
+//   //     src="${movieFastData.Poster}"
+//   //     alt=""
+//   //   />
+//   //   <p class="movie__title">${movieFastData.Title}</p>
+//   //   <div class="movie__info">
+//   //     <span class="movie__year">${movieFastData.Year}</span>
+//   //     <span class="movie__genre">${movieFastData.Genre}</span>
+//   //   </div>
+//   // </div>`)
 
 // }
 
-
-//API: https://www.omdbapi.com/?apikey=bef2d4c&s=fast
-
-
-async function allFastMovies() {
-  const movieFast = await fetch("https://www.omdbapi.com/?apikey=bef2d4c&s=fast");
-  const movieFastData = await movieFast.json();
-  console.log(movieFastData.Search)
-
-  movieFastData.Search.map(movie => {
-    console.log(movieFastData)
-  })
-
-  //   resultFastMovies.innerHTML = ` <div class="movie__img--wrapper">
-  //   <img
-  //     class="movie__img"
-  //     src="${movieFastData.Poster}"
-  //     alt=""
-  //   />
-  //   <p class="movie__title">${movieFastData.Title}</p>
-  //   <div class="movie__info">
-  //     <span class="movie__year">${movieFastData.Year}</span>
-  //     <span class="movie__genre">${movieFastData.Genre}</span>
-  //   </div>
-  // </div>`
-  
-}
-
-allFastMovies()
+// allFastMovies()
